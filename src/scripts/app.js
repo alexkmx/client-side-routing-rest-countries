a3e697d30ce94c238f3e49c27245545ffa64d3b3
Alejandro <alexk_mx@hotmail.com>
2017-10-23 13:25:54 -0500

    done

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,56 @@
-console.log('Webpack Simple Server')
-let coverEl = document.querySelector('.starter')
-let h2El = document.querySelector('h2.starter-txt')
-coverEl.className += ' loaded'
-h2El.textContent = 'Build: Webpack Simple Server'
+import { Router } from 'director/build/director';
+import request from 'superagent';
+// import showAll from '/All'
+const appContainer = document.querySelector('.countries-container')
+// appContainer.innerHTML = showAll()
+
+function showAll() {
+request.get('https://restcountries.eu/rest/v2/all')
+.then(data => {
+var info = data.body;
+
+info.forEach(function(obj) {
+  // console.log(obj);
+  var languages = obj.languages[0].nativeName
+
+  appContainer.innerHTML += `
+  <div class="country-card">
+    <img class="country-flag" src="${obj.flag}" alt="flag">
+    <h4 class="country-name">${obj.name}</h4>
+    <p class="country-capital">${obj.capital}</p>
+  </div>
+`
+})
+})
+}
+
+function showCountLang(e){
+
+request.get('https://restcountries.eu/rest/v2/all')
+.then(data => {
+var info = data.body;
+appContainer.innerHTML = ''
+info.forEach(function(obj) {
+  var nativeLang =obj.languages[0].iso639_1;
+  console.log(nativeLang);
+  if(e === nativeLang ){
+  appContainer.innerHTML += `
+  <div class="country-card">
+    <img class="country-flag" src="${obj.flag}" alt="flag">
+    <h4 class="country-name">${obj.name}</h4>
+    <p class="country-capital">${obj.capital}</p>
+  </div>
+`
+}
+})
+})
+}
+
+const routes = {
+  '/': showAll,
+  '/languages/:id': showCountLang,
+
+}
+
+const router = Router(routes);
+router.init('/');
